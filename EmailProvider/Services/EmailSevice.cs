fab4b58839449667b4ae7b6ab748d99d2677e2e9
EmmadAlsalam <143703288+EmmadAlsalam@users.noreply.github.com>
2024-05-10 10:26:46 +0200

    EmailSender är klar

---
+++ EmailProvider/Services/EmailSevice.cs
@@ -0,0 +1,52 @@
+﻿using Azure;
+using Azure.Communication.Email;
+using Azure.Messaging.ServiceBus;
+using EmailProvider.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EmailProvider.Services;
+
+public class EmailSevice(EmailClient emailclient, ILogger<EmailSevice> logger) : IEmailSevice
+{
+    private readonly EmailClient _emailClient = emailclient;
+    private readonly ILogger<EmailSevice> _logger = logger;
+
+    public EmailRequest UnPackEmailRequest(ServiceBusReceivedMessage message)
+    {
+        try
+        {
+            var emailRequest = JsonConvert.DeserializeObject<EmailRequest>(message.Body.ToString());
+            if (emailRequest != null)
+                return emailRequest;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : EmailSender.UnPackEmailRequest () ::{ex.Message} ");
+        }
+        return null!;
+    }
+
+    public bool SendEmail(EmailRequest emailRequest)
+    {
+        try
+        {
+            var result = _emailClient.Send(
+                WaitUntil.Completed,
+                senderAddress: Environment.GetEnvironmentVariable("SenderAddress"),
+                recipientAddress: emailRequest.To,
+                subject: emailRequest.Subject,
+                htmlContent: emailRequest.HtmlBody,
+                plainTextContent: emailRequest.PlainText);
+
+            if (result.HasCompleted)
+                return true;
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : EmailSender.SendEmailAsync() ::{ex.Message} ");
+        }
+        return false;
+    }
+}
---
+++ EmailProvider/Services/IEmailSevice.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Messaging.ServiceBus;
+using EmailProvider.Models;
+
+namespace EmailProvider.Services;
+
+public interface IEmailSevice
+{
+    bool SendEmail(EmailRequest emailRequest);
+    EmailRequest UnPackEmailRequest(ServiceBusReceivedMessage message);
+}
